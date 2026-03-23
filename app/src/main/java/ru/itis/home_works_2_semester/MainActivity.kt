4e8eceb002143c114140b5e08c09b16f17d478a0
Slobodenuk-Mikhail <slobodenukmikhail@gmail.com>
2026-03-23 21:20:17 +0300

    Also create project

---
+++ app/src/main/java/ru/itis/home_works_2_semester/MainActivity.kt
@@ -0,0 +1,47 @@
+package ru.itis.home_works_2_semester
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.itis.home_works_2_semester.ui.theme.Home_Works_2_SemesterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Home_Works_2_SemesterTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Home_Works_2_SemesterTheme {
+        Greeting("Android")
+    }
+}
